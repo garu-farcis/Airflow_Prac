f2d71ca856769201895700c52a7bb536834c46fa
garu-farcis <priyankagaru2024@gmail.com>
2026-09-08 02:16:39 +0200

    update

--- dags/Ladv18.py
+++ dags/Ladv18.py
@@ -9,3 +9,53 @@
    - Optionally accept a `min_count` parameter (default=1)
 
    Then build a small DAG:"""
+
+from airflow.sdk import dag,task,BaseSensorOperator
+from airflow import DAG
+import datetime
+import pandas as pd
+import os
+
+fil_path="/Users/prse/PycharmProjects/Airflow_Prac/data/support_tickets.xlsx"
+
+class NewCriticalTicketSensor(BaseSensorOperator):
+    def __init__(self,file_path,priority=None,status=None,min_count=1,**kwargs):
+        super().__init__(**kwargs)
+        self.file_path=file_path
+        self.priority=priority
+        self.status=status
+        self.min_count=min_count
+
+
+    def poke(self,context):
+        min_count=1
+        if os.path.exists(self.file_path):
+            df = pd.read_excel(self.file_path)
+            if self.priority is not None:
+                df=df[df['priority']==self.priority]
+            if self.status is not None:
+                df=df[df['status']].isin(self.status)
+            if len(df)>=min_count:
+                return True
+        return False
+
+@dag(
+    dag_id="Custom_Sensor",
+    start_date=datetime.datetime(2026,9,10),
+    schedule="@monthly",
+    catchup=False,
+    tags=['hooks'],
+)
+def my_dag():
+
+    custome_sen=NewCriticalTicketSensor(
+        task_id="custom_sensor_check",
+        file_path=fil_path,
+        status=["Open", "Escalated"],
+        priority="Critical",
+        poke_interval=30,
+        timeout=30,
+
+    )
+
+my_dag()
